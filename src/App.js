4b4986057f7a65a7e80b7d4346a67997e199a51a
vanddar4 <vanddar4@protonmail.com>
2021-03-17 00:32:55 -0400

    test git sync repo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to Leprechaun say YAYYYYY.
+          Edit <code>src/App.js</code> and save to HELLOS.
         </p>
         <a
           className="App-link"
