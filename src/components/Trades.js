4298ea0db9c438f678aa1d38407af9a721a3d319
vanddar4 <vanddar4@protonmail.com>
2021-04-04 15:52:11 -0400

    connecting trades

---
+++ src/components/Trades.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { filledOrdersLoadedSelector, filledOrdersSelector } from '../store/selectors'
+
+class Trades extends Component {
+  render() {
+    return (
+      <div className="card bg-dark text-white">
+      <div className="card-header">
+        Card Title
+      </div>
+      <div className="card-body">
+        <table className="table table-dark table-sm small">
+          <thead>
+            <tr>
+              <th>Time</th>
+              <th>DYRIO</th>
+              <th>DYRIO/ETH</th>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    filledOrdersLoaded: filledOrdersLoadedSelector(state),
+    filledOrders: filledOrdersSelector(state),
+  }
+}
+
+export default connect(mapStateToProps)(Trades)
